9227ca2f6a649676855b3ef879c4877f732f7df6
Carlton CJ Francis <carlton.francis@blackswan.com>
2019-04-29 14:53:51 -0700

    Added basic Navbar, Landing, and Footer components

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      Copyright &copy; {new Date().getFullYear()} {/* TODO: COMPANY NAME */}
+    </footer>
+  );
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,4 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { App } from "./components/App";
-ReactDOM.render(
-  <App compiler="Typescript" framework="React" bundler="Webpack" />,
-  document.getElementById("root")
-);
+import App from "./components/App";
+ReactDOM.render(<App />, document.getElementById("root"));
